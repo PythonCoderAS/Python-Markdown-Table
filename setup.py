8645f188546d709f85e70a1565236eb82c4a5842
PokestarFan <azoysarkar@gmail.com>
2017-11-03 19:06:57 -0400

    Uploading changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='markdowntable',
-      version='0.1',
+      version='2.0',
       description='Easy way to make markdown code for tables',
       url='https://github.com/PokestarFan/Python-Markdown-Table',
       author='PokestarFan',
