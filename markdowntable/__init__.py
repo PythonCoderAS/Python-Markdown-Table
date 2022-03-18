4fc7ab2385c22238124485738c8f0ef3c4ba4b14
PythonCoderAS <sarkaraoyan@gmail.com>
2022-03-17 22:20:55 -0400

    Add rewrite version

--- markdowntable/__init__.py
+++ markdowntable/__init__.py
@@ -1,5 +1,20 @@
-from __future__ import print_function, unicode_literals
+from dataclasses import dataclass, field
+from typing import Iterable
 
-import markdowntable.errors
-from markdowntable.row import Row, Column
-from markdowntable.table import Table
+
+@dataclass
+class Table:
+    headers: list[str] = field(default_factory=list)
+    rows: list[Iterable[str]] = field(default_factory=list, init=False)
+
+    def add_row(self, row: Iterable[str]):
+        self.rows.append(row)
+
+    def set_headers(self, headers: list[str]):
+        self.headers = headers
+
+    def get_table(self) -> str:
+        header_row = "|" + "|".join(self.headers) + "|"
+        separator_row = "|" + "|".join(["-"] * len(self.headers)) + "|"
+        body_rows = [f"|{'|'.join(row)}|" for row in self.rows]
+        return "\n".join([header_row, separator_row] + body_rows)
